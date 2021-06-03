62fe06543dfe3a44bcdbda4e19e9ab4df07429d3
Leony Novena <leonynovena178@gmail.com>
2021-06-03 11:06:20 +0700

    update db, buat routes

--- db_proyek_soa.sql
+++ db_proyek_soa.sql
@@ -1,60 +1,72 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: May 31, 2021 at 01:23 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.13-MariaDB : Database - db_proyek_soa
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_proyek_soa` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
---
--- Database: `db_proyek_soa`
---
-CREATE DATABASE IF NOT EXISTS `db_proyek_soa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `db_proyek_soa`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `buku`
---
+/*Table structure for table `buku` */
 
 DROP TABLE IF EXISTS `buku`;
+
 CREATE TABLE `buku` (
-  `id_buku` varchar(10) NOT NULL,
-  `id_perpus` varchar(10) NOT NULL,
-  `id_buku_api` varchar(20) NOT NULL
+  `id_buku` varchar(5) NOT NULL,
+  `id_perpus` varchar(5) NOT NULL,
+  `id_buku_api` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_buku`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `buku`
---
+/*Table structure for table `buku_perpus` */
 
-INSERT INTO `buku` (`id_buku`, `id_perpus`, `id_buku_api`) VALUES
-('B001', 'P003', 'buc0AAAAMAAJ'),
-('B002', 'P002', 'buc0AAAAMAAJ');
+DROP TABLE IF EXISTS `buku_perpus`;
 
---
--- Indexes for dumped tables
---
+CREATE TABLE `buku_perpus` (
+  `id_buku` varchar(5) NOT NULL,
+  `id_perpus` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for table `buku`
---
-ALTER TABLE `buku`
-  ADD PRIMARY KEY (`id_buku`);
-COMMIT;
+/*Table structure for table `peminjaman` */
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS `peminjaman`;
+
+CREATE TABLE `peminjaman` (
+  `id_pinjam` varchar(5) NOT NULL,
+  `id_perpus` varchar(5) NOT NULL,
+  `id_buku` varchar(5) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  PRIMARY KEY (`id_pinjam`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id_user` varchar(5) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(200) DEFAULT NULL COMMENT 'khusus P',
+  `kota` varchar(20) DEFAULT NULL COMMENT 'khusus P',
+  `no_telepon` varchar(15) DEFAULT NULL COMMENT 'khusus P',
+  `api_hit` int(2) NOT NULL,
+  `saldo` int(5) NOT NULL,
+  `role` varchar(1) NOT NULL COMMENT 'P: perpus || U: umum',
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../conn');
+const axios = require('axios');
+const multer = require('multer');
+
+//register
+router.post('/register', (req, res) => {
+
+});
+
+//login
+router.post('/login', (req, res) => {
+
+});
+
+module.exports = router;
