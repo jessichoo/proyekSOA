d7996eb0571e094aa6b137ed7224d213ee18ca57
Vinny Tantri <vinny1@mhs.stts.edu>
2021-07-03 21:21:09 +0700

    pindah ke route

--- routes/buku.js
+++ routes/buku.js
@@ -57,6 +57,99 @@ router.get('/', async (req, res) => {
 // - beli buku
 // - lihat request buku (perpus dan user)
 
+//lihat preview buku
+router.post("/preview/:judul", async (req,res)=>{
+    const token = req.header("x-auth-token");
+    if(!token){
+        return res.status(401).send({"msg":"token tidak ditemukan!"});
+    }
+
+    let user={};
+    try {
+        user = jwt.verify(token,"proyeksoa");
+    } catch (error) {
+        return res.status(400).send({"msg":"token tidak valid!"});
+    }
+
+    let judul = req.params.judul;
+    let conn = await db.getConn();
+    let cariBuku = await db.executeQuery(`SELECT * FROM buku WHERE LOWER(buku) = '${judul.toLocaleLowerCase()}'`);
+    conn.release();
+    if(!cariBuku.length){
+        return res.status(404).json({
+            message: 'Buku yang anda cari tidak tersedia',
+            status_code: 404
+        });
+    }
+    let search = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${judul}&key=AIzaSyCh9du1IyImJP4TjJ2Qj6wasDMvhsz0RlI`);
+    let result = search.data.items;
+    let hasil = [];
+    result.forEach(element => {
+        console.log(element);
+        let buku={
+            "preview_link" :element.accessInfo.webReaderLink
+        };
+        hasil.push(buku);
+    });
+    
+    let today = new Date();
+    let date = "";
+    if((today.getMonth()+1) < 10 && today.getDate() < 10){
+        date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-0'+today.getDate();
+    } else if ((today.getMonth()+1) < 10) {
+        date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+today.getDate();
+    } else if (today.getDate() < 10){
+        date = today.getFullYear()+'-'+(today.getMonth()+1)+'-0'+today.getDate();
+    } else {
+        date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    }
+    
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+
+    conn = await db.getConn();
+    let cekAccess = await db.executeQuery(conn, `SELECT * FROM access_log WHERE id_user = '${user.id_user}' AND access_time LIKE '${date}%'`);
+    conn.release();
+
+    //console.log(cekAccess.length);
+    if(cekAccess.length<2){
+        if(user.api_hit>=1){
+            conn = await db.getConn();
+            let minApiHit = await db.executeQuery(conn, `UPDATE user SET api_hit = api_hit-1 WHERE id_user = '${user.id_user}'`);
+        } else {
+            return res.status(400).json({
+                message: 'Api hit anda tidak cukup',
+                status_code: 400
+            });
+        }
+    } else {
+        if(user.saldo>=10000){
+            conn = await db.getConn();
+            let minSaldo = await db.executeQuery(conn, `UPDATE user SET saldo = saldo-10000 WHERE id_user = '${user.id_user}'`);
+        } else {
+            return res.status(400).json({
+                message: 'Saldo anda tidak cukup',
+                status_code: 400
+            });
+        }
+        conn.release();
+    }
+
+    conn = await db.getConn();
+    let inputLog = await db.executeQuery(conn,`INSERT INTO access_log VALUES (null, '${user.id_user}','${dateTime}')`);
+    conn.release();
+
+    conn = await db.getConn();
+    let tambahPreview = await db.executeQuery(`UPDATE buku SET preview = preview + 1 WHERE id_buku = '${cariBuku[0].id}'`);
+    conn.release();
+
+    //console.log(hasil);
+    return res.status(200).json({
+        link_preview:hasil[0],
+        status_code: 200,
+    });
+
+})
 
 // add ke tabel buku dan buku_perpus
 router.post("/add", async(req, res) => {
