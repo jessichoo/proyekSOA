e9e07cfe3dba6fbaaef0495bbd713e1abb872ed1
Jessica Nathania <jessicanathaniawidjaja@gmail.com>
2021-07-03 21:50:03 +0800

    sql

---
+++ buku.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 03, 2021 at 03:49 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_proyek_soa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` varchar(50) NOT NULL,
+  `isbn` varchar(30) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `tahun` varchar(50) DEFAULT NULL,
+  `genre` varchar(100) DEFAULT NULL,
+  `preview` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `isbn`, `judul`, `author`, `tahun`, `genre`, `preview`) VALUES
+('B3qSvwEACAAJ', '9780062224149', 'Broken Things', 'Lauren Oliver', '2019-09-03', 'Young Adult Fiction', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
